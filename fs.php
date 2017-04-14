b342d4c9832baedaade2b30b33f6c61c06f0a0bc
zheka <poliv78@gmail.com>
2017-04-14 14:40:30 +0300

    not_found xml

--- fs.php
+++ fs.php
@@ -7,7 +7,32 @@ $not_found = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
   </section>
 </document>';
 
-
+if ($request["action"]=="sip_auth"){
+    $login = $request["sip_auth_username"];
+    if ($login=="test"){
+        $reply = '<document type="freeswitch/xml">
+          <section name="directory">
+            <domain name="'.$request['domain'].'">
+              <params>
+                <param name="dial-string" value="{presence_id=${dialed_user}@${dialed_domain}}${sofia_contact(${dialed_user}@${dialed_domain})}"/>
+              </params>
+              <groups>
+                <group name="default">
+                 <users>
+                  <user id="'.$login.'">
+                    <params>
+                      <param name="password" value="some_password"/>
+                    </params>
+                  </user>
+                 </users>
+                </group>
+              </groups>
+            </domain>
+          </section>
+        </document>';
+        die();
+    }
+}
 
 
 echo $not_found;
