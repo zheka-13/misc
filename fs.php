d6a62f8f50b4d42be7d1bf5b39487419c69fc9ae
zheka <poliv78@gmail.com>
2017-04-14 14:00:13 +0300

    not_found xml

--- fs.php
+++ fs.php
@@ -1,5 +1,14 @@
 <?php
-error_log(print_r($_POST, true));
-error_log(print_r($_GET, true));
+$request  = $_POST;
+$not_found = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<document type="freeswitch/xml">
+  <section name="result">
+    <result status="not found" />
+  </section>
+</document>';
 
+
+
+
+echo $not_found;
 ?>
